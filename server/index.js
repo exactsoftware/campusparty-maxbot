6b14749aba0b85509303f85a166282f233cb8bdc
Vincent Lesierse <vincent@lesierse.com>
2016-05-12 13:34:12 +0200

    Added sample questions and answers

--- server/index.js
+++ server/index.js
@@ -7,7 +7,17 @@ var app = express();
 app.get('/', function (req, res) {
   res.contentType("text/plain");
   if (req.query.question) {
-    res.send("Ok, I don't understand your question '" + req.query.question + "'.");
+    if (req.query.question === "What time do you start the qualifying race of the Formula 1 Monaco?") {
+      res.send("13:00");
+    } else if (req.query.question === "What do you think of the Formula 1 Russia?") {
+      res.send("Sochi is an interesting track: a circuit with room to overtake. Hopefully we don’t need to but if it’s necessary, there are opportunities.");
+    } else if (req.query.question === "For which team do you race?") {
+      res.send("Scuderia Toro Rosso");
+    } else if (req.query.question === "Which place did you end in China?") {
+      res.send("8th");
+    } else {
+      res.send("Ok, I don't understand your question '" + req.query.question + "'.");
+    }
   } else {
     res.send("Hi, I'm Max Verstappen. Do you have a question for me?");
   }
--- test/index.js
+++ test/index.js
@@ -23,4 +23,36 @@ describe('GET /?question', function() {
       .expect(200)
       .expect("Ok, I don't understand your question 'test'.", done);
   });
+  it('respond with 1st sample question', function(done) {
+    request(app)
+      .get('/?question=What+time+do+you+start+the+qualifying+race+of+the+Formula+1+Monaco?')
+      .set('Accept', 'text/plain')
+      .expect('Content-Type', /plain/)
+      .expect(200)
+      .expect("13:00", done);
+  });
+  it('respond with 2nd sample question', function(done) {
+    request(app)
+      .get('/?question=What+do+you+think+of+the+Formula+1+Russia?')
+      .set('Accept', 'text/plain')
+      .expect('Content-Type', /plain/)
+      .expect(200)
+      .expect("Sochi is an interesting track: a circuit with room to overtake. Hopefully we don’t need to but if it’s necessary, there are opportunities.", done);
+  });
+  it('respond with 3rd sample question', function(done) {
+    request(app)
+      .get('/?question=For+which+team+do+you+race?')
+      .set('Accept', 'text/plain')
+      .expect('Content-Type', /plain/)
+      .expect(200)
+      .expect("Scuderia Toro Rosso", done);
+  });
+  it('respond with 4th sample question', function(done) {
+    request(app)
+      .get('/?question=Which+place+did+you+end+in+China?')
+      .set('Accept', 'text/plain')
+      .expect('Content-Type', /plain/)
+      .expect(200)
+      .expect("8th", done);
+  });
 });
